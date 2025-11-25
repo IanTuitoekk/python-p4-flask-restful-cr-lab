2b113cd30f328cb0df6d497e2ca7257f558a7da7
Ian Tuitoek <iantuitoek4@gmail.com>
2025-11-25 13:17:05 +0300

    Implement CRUD operations for Plants resource and update database schema
    - Added GET and POST methods to the Plants resource for retrieving all plants and adding a new plant.
    - Implemented GET method in PlantByID resource to retrieve a plant by its ID.
    - Created a new migration script to add columns (name, image, price) to the plants table.
    - Updated the Plant model to include new attributes corresponding to the database schema.

--- server/models.py
+++ server/models.py
@@ -7,3 +7,6 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60))
+    image = db.Column(db.String(60))
+    price = db.Column(db.Numeric)
